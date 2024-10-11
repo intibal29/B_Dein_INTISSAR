d6aae6048196dc6099d0eb2737c19ac82d602c10
Intissar <baloukintissar@gmail.com>
2024-10-11 12:15:39 +0200

    hello aplication

--- src/main/java/org/example/b/HelloApplication.java
+++ src/main/java/org/example/b/HelloApplication.java
@@ -4,20 +4,28 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.Parent;
 
-import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+        // Carga del archivo FXML usando una ruta relativa
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/org/example/b/hello-view.fxml")));
+
+        primaryStage.setTitle("Gestión de Personas");
+        primaryStage.setScene(new Scene(root, 600, 400));
+
+        // Cargar el icono de la aplicación usando una ruta relativa
+        primaryStage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/agenda.png"))));
+
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
